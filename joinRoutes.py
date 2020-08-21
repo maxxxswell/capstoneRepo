bcf8025c466973da2461c9221cd9c1c86e082680
maxxxswell <maxmcdonald014@gmail.com>
2020-08-21 13:50:59 -0400

    more formatting

--- joinRoutes.py
+++ joinRoutes.py
@@ -1,22 +1,28 @@
-# import modules
+# Author: Max McDonald
+# Name: joinRoutes
+# Description: Walks through files in gdb and merges all
+# files that are polylines before saving merged files to 
+# a new location
+
+# import arcpy and os modules
 import arcpy
 import os
 
-# set workspace for all routes
+# set environments
 arcpy.env.workspace = r"myRoutes.gdb"
 
 # create an empty list
 featureClasses = []
 
-# walk through geodatabase for filename in filenames
+# walk through gdb files
 for dirpath, dirnames, filenames in arcpy.da.Walk(arcpy.env.workspace, datatype="FeatureClass", type="Polyline"):
     for filename in filenames:
-        # describe it 
+        # describe the current feature class
         desc = arcpy.Describe(os.path.join(dirpath, filename))
-        # if describe shapetype is polyline
+        # if describe shapetype is polyline...
         if desc.shapeType == "Polyline":
             # add it to list
             featureClasses.append(os.path.join(dirpath, filename))
 
-# merge all list items to destination
+# merge all feature classes in list 
 arcpy.Merge_management(featureClasses, r"myDestination/myFileName)
